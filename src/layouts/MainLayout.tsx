cd974cfdf53e02a45a35fff8d0dabad2dfabdf42
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-09-12 18:32:16 +0330

    Redirect when access token is not exist

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { getCookie } from "../utils/cookiesManagement";
 
 function MainLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = getCookie("accessToken");
+    if (!accessToken) {
+      //redirect
+      navigate("auth/login", { replace: true });
+    }
+  }, []);
+
   return (
     <div>
       MainLayout
