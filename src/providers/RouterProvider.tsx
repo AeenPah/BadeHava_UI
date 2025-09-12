b18fdc20efe96af42ec3ebfc99915f216064341c
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-09-12 18:09:17 +0330

    Initialize react router for auth route

---
+++ src/providers/RouterProvider.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/auth/login/Login";
+import MainLayout from "../layouts/MainLayout";
+import Register from "../pages/auth/login/Register";
+
+function RouterProvider() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="auth" element={<AuthLayout />}>
+          <Route index element={<Navigate to="login" replace />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+
+        <Route path="/" element={<MainLayout />}></Route>
+
+        <Route path="*" element={<div>Page not found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RouterProvider;
