b167dd3b9272d958e585851006e606f13ec2d92d
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-09-14 13:28:12 +0330

    Add call refresh api on 401 error

--- src/lib/AxiosInstance.ts
+++ src/lib/AxiosInstance.ts
@@ -1,30 +1,56 @@
 import axios from "axios";
+import { setCookie } from "../utils/cookiesManagement";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
 const AXIOS = axios.create({
   baseURL,
-  //   timeout: 5000,
 });
 
 AXIOS.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (!error.response) return Promise.reject(error);
+  async (error) => {
+    if (!error.response) {
+      return Promise.reject(error);
+    }
 
+    // FIXME: Fix this type later
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const originalRequest = error.config as any;
     const { status } = error.response;
 
-    switch (status) {
-      case 401:
-        alert("Unauthorized!");
-        break;
-      case 500:
-        alert("Server Error");
-        break;
-      default:
-        // FIXME: remove this line later
-        alert(error.response.data.message);
-        break;
+    if (status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const { data } = await axios.post(
+          `${baseURL}/auth/refresh`,
+          undefined,
+          {
+            withCredentials: true,
+          }
+        );
+
+        setCookie("accessToken", data.data.accessToken);
+        setCookie("username", data.data.username);
+
+        // Attach new token to original request and retry
+        originalRequest.headers.Authorization = `Bearer ${data.data.accessToken}`;
+        return AXIOS(originalRequest);
+      } catch (err: unknown) {
+        if (axios.isAxiosError(err)) {
+          if (err.response?.status === 401) {
+            console.log("Redirect to login page");
+            // window.location.href = "/auth/login";
+          }
+        }
+        return Promise.reject(err);
+      }
+    }
+
+    if (status === 500) {
+      alert("Server Error. Try again later.");
+    } else if (error.response.data?.message) {
+      alert(error.response.data.message);
     }
 
     return Promise.reject(error);
