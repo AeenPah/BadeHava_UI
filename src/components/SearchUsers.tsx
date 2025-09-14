4c0cdf74e6bca18b07f62648c53611fe26aa476d
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-09-14 16:13:21 +0330

    Make search Users component

---
+++ src/components/SearchUsers.tsx
@@ -0,0 +1,72 @@
+import { Fragment, useEffect, useRef, useState, type ChangeEvent } from "react";
+import { getCookie } from "../utils/cookiesManagement";
+import AXIOS from "../lib/AxiosInstance";
+
+type TUserType = { userId: number; username: string };
+
+function SearchUsers() {
+  /* -------------------------------------------------------------------------- */
+  /*                                 React Hooks                                */
+  /* -------------------------------------------------------------------------- */
+
+  const [searchInput, setSearchInput] = useState<string | null>(null);
+  const [foundUsers, setFoundUsers] = useState<TUserType[] | null>(null);
+
+  const searchRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (searchRef.current) {
+      clearTimeout(searchRef.current);
+    }
+
+    searchRef.current = setTimeout(() => {
+      if (!searchInput) {
+        setFoundUsers(null);
+        return;
+      }
+
+      const data = { searchInput };
+      AXIOS.post("user/search", data, {
+        headers: {
+          Authorization: `Bearer ${getCookie("accessToken")}`,
+        },
+        withCredentials: true,
+      }).then(({ data }) => {
+        let users: TUserType[] = data.data.users;
+        if (!users.length) {
+          users = [{ userId: 0, username: "No User found!" }];
+        }
+
+        setFoundUsers(users);
+      });
+    }, 200);
+  }, [searchInput]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  Functions                                 */
+  /* -------------------------------------------------------------------------- */
+
+  function searchOnChange(event: ChangeEvent<HTMLInputElement>) {
+    setSearchInput(event.target.value);
+  }
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        name="searchUser"
+        id="searchUser"
+        placeholder="Search Users ..."
+        onChange={(e) => searchOnChange(e)}
+      />
+      {foundUsers &&
+        foundUsers.map((u) => (
+          <div key={u.username}>
+            {u.userId}- {u.username}
+          </div>
+        ))}
+    </Fragment>
+  );
+}
+
+export default SearchUsers;
--- src/pages/main/home/Home.tsx
+++ src/pages/main/home/Home.tsx
@@ -1,54 +1,11 @@
-import { useEffect, useRef, useState, type ChangeEvent } from "react";
 import AXIOS from "../../../lib/AxiosInstance";
 import { getCookie, setCookie } from "../../../utils/cookiesManagement";
-
-type TUserType = { userId: number; username: string };
+import SearchUsers from "../../../components/SearchUsers";
 
 function Home() {
   /* -------------------------------------------------------------------------- */
-  /*                                 React Hooks                                */
-  /* -------------------------------------------------------------------------- */
-
-  const [searchInput, setSearchInput] = useState<string | null>(null);
-  const [foundUsers, setFoundUsers] = useState<TUserType[] | null>(null);
-
-  const searchRef = useRef<number | null>(null);
-
-  useEffect(() => {
-    if (searchRef.current) {
-      clearTimeout(searchRef.current);
-    }
-
-    searchRef.current = setTimeout(() => {
-      if (!searchInput) {
-        setFoundUsers(null);
-        return;
-      }
-
-      const data = { searchInput };
-      AXIOS.post("user/search", data, {
-        headers: {
-          Authorization: `Bearer ${getCookie("accessToken")}`,
-        },
-        withCredentials: true,
-      }).then(({ data }) => {
-        let users: TUserType[] = data.data.users;
-        if (!users.length) {
-          users = [{ userId: 0, username: "No User found!" }];
-        }
-
-        setFoundUsers(users);
-      });
-    }, 200);
-  }, [searchInput]);
-
-  /* -------------------------------------------------------------------------- */
   /*                                  Functions                                 */
   /* -------------------------------------------------------------------------- */
-
-  function searchOnChange(event: ChangeEvent<HTMLInputElement>) {
-    setSearchInput(event.target.value);
-  }
 
   function logoutOnclick() {
     AXIOS.post("auth/logout", undefined, {
@@ -67,19 +24,7 @@ function Home() {
     <div>
       <h4>Home</h4>
       <button onClick={logoutOnclick}>Logout</button> <br />
-      <input
-        type="text"
-        name="searchUser"
-        id="searchUser"
-        placeholder="Search Users ..."
-        onChange={(e) => searchOnChange(e)}
-      />
-      {foundUsers &&
-        foundUsers.map((u) => (
-          <div key={u.username}>
-            {u.userId}- {u.username}
-          </div>
-        ))}
+      <SearchUsers />
     </div>
   );
 }
